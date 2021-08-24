2767083c762a8c11ad6fdbb9311f1be6deeebd5b
RogerioMattos <rogerio.mattos@stoom.com.br>
2021-08-24 18:34:43 -0300

    criando estrutura

---
+++ web/src/pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>It's me Mario!</title>
+        <meta name="description" content="Site Mario" />
+      </Head>
+
+      <main>
+       <h1>It's me Mario!</h1>
+      </main>
+
+      <footer>
+        
+      </footer>
+    </div>
+  )
+}
